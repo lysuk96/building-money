abd625481dff1575be3d1faaa2850b50afebca78
이영석 <2016025732@hanyang.ac.kr>
2022-05-05 20:52:47 +0900

    fix: result card size to 80%

--- webpack-react/src/component/Card.js
+++ webpack-react/src/component/Card.js
@@ -17,7 +17,7 @@ export const ResultCardWrapper = styled.div`
   overflow: visible;
   padding: 0 0 32px;
   margin: 48px auto 0;
-  width: 70%;
+  width: 80%;
   font-family: Quicksand, arial, sans-serif;
   box-shadow: 0 0 20px rgba(0, 0, 0, 0.05), 0 0px 40px rgba(0, 0, 0, 0.08);
   border-radius: 5px;
