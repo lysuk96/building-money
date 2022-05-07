57975d1cce1e7ee21bd73d0955c42d40583b316f
이영석 <2016025732@hanyang.ac.kr>
2022-05-07 21:10:58 +0900

    fix: check public img error

--- webpack-react/src/dummy/RewardDummy.js
+++ webpack-react/src/dummy/RewardDummy.js
@@ -3,27 +3,27 @@ export default
     {
         profit: [0, 25],
         label: "에어팟이 하나",
-        img: "/image/airpod.jpeg"
+        img: "/airpod.jpeg"
     },
     {
         profit: [25, 50],
         label: "에어조던이 한켤레",
-        img: "/image/airjordan.jpg"
+        img: "/airjordan.jpg"
     },
     {
         profit: [50, 75],
         label: "일본 초호화 여행 쌉가능",
-        img: "/image/japan.jpg"
+        img: "/japan.jpg"
     },
     {
         profit: [100, 150],
         label: "맥북이 한대",
-        img: "/image/macbook.jpg"
+        img: "/macbook.jpg"
     },
     {
         profit:[150, 200],
         label: "베스파가 한대",
-        img: "/image/vespa.jpg"
+        img: "/vespa.jpg"
     }
 
 ]
