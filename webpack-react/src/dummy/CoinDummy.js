ca8f5f27a8bbb6f2b947ad4ae998e1b31f07762b
이영석 <2016025732@hanyang.ac.kr>
2022-05-09 14:38:40 +0900

    update: coin dummy data

--- webpack-react/src/dummy/CoinDummy.js
+++ webpack-react/src/dummy/CoinDummy.js
@@ -2,6 +2,7 @@ export default
 [
     { label: "BTC", value: 0 },
     { label: "ETH", value: 1 },
-    { label: "DOGE", value: 2 },
-    { label: "KLAY", value: 3 },
+    { label: "ADA", value: 2 },
+    { label: "XRP", value: 3 },
+    { label: "DOGE", value: 4 },
 ]
