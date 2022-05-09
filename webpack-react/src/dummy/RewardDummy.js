e51adfa503f40bbe0725bec0d858f2d07fafc840
이영석 <2016025732@hanyang.ac.kr>
2022-05-09 14:50:02 +0900

    update: reward dummy data

--- webpack-react/src/dummy/RewardDummy.js
+++ webpack-react/src/dummy/RewardDummy.js
@@ -2,27 +2,27 @@ export default
 [
     {
         profit: [0, 25],
-        label: "에어팟이 하나",
+        label: "에어팟 하나 더 사는건데",
         img: "/airpod.jpeg"
     },
     {
         profit: [25, 50],
-        label: "에어조던이 한켤레",
+        label: "에어조던 한 켤레 더 사는건데",
         img: "/airjordan.jpg"
     },
     {
         profit: [50, 75],
-        label: "일본 초호화 여행 쌉가능",
+        label: "일본 초호화 여행 쌉가능인데",
         img: "/japan.jpg"
     },
     {
         profit: [100, 150],
-        label: "맥북이 한대",
+        label: "맥북 한 대 더 사는건데",
         img: "/macbook.jpg"
     },
     {
         profit:[150, 200],
-        label: "베스파가 한대",
+        label: "베스파 한 대 더 사는건데",
         img: "/vespa.jpg"
     }
 
